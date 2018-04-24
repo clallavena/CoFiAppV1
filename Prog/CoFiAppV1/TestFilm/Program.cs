00d27e66ab98050c00aa021547cb9d976f583436
clallavena <clallavena@1ddcc0a7-e0ef-4e35-b477-2a29d5999e86>
2018-04-24 13:24:47 +0000

    Ajout du debut du code, classe Acteur et Film, + fin uml

---
+++ Prog/CoFiAppV1/TestFilm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Metier;
+
+namespace TestFilm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Film f = new Film("2001", 1989, null, "Esteban le con");
+            Console.WriteLine(f);
+        }
+    }
+}
