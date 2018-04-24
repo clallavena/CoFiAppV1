7d695e732eacd5bf29a82a69fc37a7f56d86d549
clallavena <clallavena@1ddcc0a7-e0ef-4e35-b477-2a29d5999e86>
2018-04-24 13:24:47 +0000

    Ajout du debut du code, classe Acteur et Film, + fin uml

---
+++ trunk/Prog/CoFiAppV1/Metier/Acteur.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metier
+{
+    public class Acteur
+    {
+
+        public string Nom
+        {
+            get; set;
+        }
+
+        public string Prenom
+        {
+            get; set;
+        }
+
+        public Acteur(string nom, string prenom)
+        {
+            Nom = nom;
+            Prenom = prenom;
+        }
+
+        public override string ToString()
+        {
+            return $"Nom: {Nom}, Prenom: {Prenom}";
+        }
+        
+    }
+}
---
+++ trunk/Prog/CoFiAppV1/Metier/Film.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metier
+{
+    public class Film
+    {
+        public string Titre
+        {
+            get; set;
+        }
+
+        public int DateDeSortie
+        {
+            get; set;
+        }
+
+        public List<String> ListTags
+        {
+            get; set;
+        }
+
+        public string Synopsis
+        {
+            get; set;
+        }
+
+        public Film(string titre, int dateDeSortie, List<string> listTags,string synopsis)
+        {
+            Titre = titre;
+            DateDeSortie = dateDeSortie;
+            ListTags = listTags;
+            Synopsis = synopsis;
+        }
+
+        public override string ToString()
+        {
+            return $"{Titre} {DateDeSortie} {ListTags} {Synopsis}";
+        }
+    }
+}
