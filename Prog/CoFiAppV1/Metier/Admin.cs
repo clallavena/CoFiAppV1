8550e84dc0900d5be67a6147cec93e211d5ba906
esbarland <esbarland@1ddcc0a7-e0ef-4e35-b477-2a29d5999e86>
2018-05-04 07:52:19 +0000

    ajout des commentaires dans les classes du métier et redéfinition de ma méthode equals pour la classe Acteur

--- Prog/CoFiAppV1/Metier/Admin.cs
+++ Prog/CoFiAppV1/Metier/Admin.cs
@@ -6,43 +6,86 @@ using System.Threading.Tasks;
 
 namespace Metier
 {
+    /// <summary>
+    /// La classe Admin représente un utilisateur avec des droits supplémentaires. Il comporte un nom d'utilisateur et un mot de passe
+    /// </summary>
     public class Admin
     {
+        /// <summary>
+        /// Nom d'utilisateur de l'administrateur
+        /// </summary>
         public string Username {
             get; private set;
         }
 
+        /// <summary>
+        /// Mot de passe de l'administrateur
+        /// </summary>
         public string Password
         {
             get; private set;
         }
 
+        /// <summary>
+        /// Constructeur d'un administrateur avec un nom d'utilisateur et d'un mot de passe
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
         public Admin(string username, string password)
         {
             Username = username;
             Password = password;
         }
 
+        /// <summary>
+        /// Méthode permettant à un administrateur de supprimer un film
+        /// </summary>
+        /// <param name="film"></param>
         public void SupprimerFilm(Film film)
         {
 
         }
 
+        /// <summary>
+        /// Méthode permettant à un administrateur de supprimer un réalisateur
+        /// </summary>
+        /// <param name="real"></param>
         public void SupprimerReal(Realisateur real)
         {
 
         }
 
+        /// <summary>
+        /// Méthode permettant à un administrateur de modifier les informations d'un film
+        /// </summary>
+        /// <param name="film"></param>
+        /// <param name="titre"></param>
+        /// <param name="dateDeSortie"></param>
+        /// <param name="synopsis"></param>
+        /// <param name="listTags"></param>
         public void ModifierFilm(Film film, string titre, int dateDeSortie, string synopsis, params string[] listTags)
         {
 
         }
 
+        /// <summary>
+        /// Méthode permettant à un administrateur de modifier les informations d'un réalisateur
+        /// </summary>
+        /// <param name="real"></param>
+        /// <param name="nom"></param>
+        /// <param name="prenom"></param>
+        /// <param name="dateDeNaissance"></param>
+        /// <param name="dateDeMort"></param>
+        /// <param name="nationalite"></param>
+        /// <param name="biographie"></param>
         public void ModiferReal(Realisateur real, string nom, string prenom, DateTime dateDeNaissance, DateTime dateDeMort, string nationalite, string biographie)
         {
 
         }
 
+        /// <summary>
+        /// Rédéfinition de la méthode ToString pour un meilleur affichage
+        /// </summary>
         public override string ToString()
         {
             return $"Username: {Username} Password: {Password}";
--- Prog/CoFiAppV1/Metier/Realisateur.cs
+++ Prog/CoFiAppV1/Metier/Realisateur.cs
@@ -6,48 +6,67 @@ using System.Threading.Tasks;
 
 namespace Metier
 {
+    /// <summary>
+    /// La classe Réalisateur est composé d'un nom, d'un prénom, d'une date de naissance, d'une date de mort (optionnel), d'une nationalité et d'une biographie
+    /// </summary>
     public class Realisateur
     {
+        /// <summary>
+        /// Nom du réalisateur
+        /// </summary>
         public string Nom
         {
             get; private set;
         }
 
+        /// <summary>
+        /// Prénom du réalisateur
+        /// </summary>
         public string Prenom
         {
             get; private set;
         }
 
+        /// <summary>
+        /// Date de naissance de l'utilisateur
+        /// </summary>
         public DateTime DateDeNaissance
         {
             get; private set;
         }
 
-        public DateTime DateDeMort
+        /// <summary>
+        /// Date de mort de l'utilisateur (optionnel)
+        /// </summary>
+        public DateTime? DateDeMort
         {
             get; private set;
         }
 
+        /// <summary>
+        /// Nationalité de l'utilisateur
+        /// </summary>
         public string Nationalite
         {
             get; private set;
         }
 
+        /// <summary>
+        /// Biographie de l'utilisateur
+        /// </summary>
         public string Biographie
         {
             get; private set;
         }
 
-        public Realisateur(string nom, string prenom, DateTime dateDeNaissance, DateTime dateDeMort, string nationalite, string biographie)
-        {
-            Nom = nom;
-            Prenom = prenom;
-            DateDeNaissance = dateDeNaissance;
-            DateDeMort = dateDeMort;
-            Nationalite = nationalite;
-            Biographie = biographie;
-        }
-
+        /// <summary>
+        /// Constructeur d'un réalisateur sans date de mort
+        /// </summary>
+        /// <param name="nom"></param>
+        /// <param name="prenom"></param>
+        /// <param name="dateDeNaissance"></param>
+        /// <param name="nationalite"></param>
+        /// <param name="biographie"></param>
         public Realisateur(string nom, string prenom, DateTime dateDeNaissance, string nationalite, string biographie)
         {
             Nom = nom;
@@ -55,16 +74,17 @@ namespace Metier
             DateDeNaissance = dateDeNaissance;
             Nationalite = nationalite;
             Biographie = biographie;
+            DateDeMort = null;
         }
 
+        public Realisateur(string nom, string prenom, DateTime dateDeNaissance, DateTime dateDeMort, string nationalite, string biographie) : this(nom, prenom, dateDeNaissance, nationalite, biographie)
+        {
+            DateDeMort = dateDeMort;
+        }
+        
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
-
-
-            int year = DateDeNaissance.Year;
-            int month = DateDeNaissance.Month;
-            int day = DateDeNaissance.Day;
 
             sb.Append(Nom);
             sb.Append(" ");
@@ -72,10 +92,9 @@ namespace Metier
             sb.Append(" ");
             sb.Append(DateDeNaissance.ToShortDateString());
 
-            if(!DateDeMort.Equals(new DateTime(0001, 01, 01)))
-            {
+            if(DateDeMort.HasValue)            {
                 sb.Append(" ");
-                sb.Append(DateDeMort.ToShortDateString());
+                sb.Append(DateDeMort.Value.ToShortDateString());
             }
 
             sb.Append(" ");
