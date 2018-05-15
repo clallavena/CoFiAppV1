e5a1f4bc83099094cb25053a6c38c75be105fe72
esbarland <esbarland@1ddcc0a7-e0ef-4e35-b477-2a29d5999e86>
2018-05-15 07:53:13 +0000

    ajout des classes pour rechercher des films

---
+++ Prog/CoFiAppV1/Metier/IRecherchePersonneParCritere.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metier
+{
+    public interface IRecherchePersonneParCritere
+    {
+
+        Personne RechercherRealisateur(string nom);
+
+        Personne RechercherRealisateur(string nom, string prenom);
+
+    }
+}
