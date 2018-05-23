1d5bed29ee4adabcd02456d9d34569b78b3900c6
esbarland <esbarland@1ddcc0a7-e0ef-4e35-b477-2a29d5999e86>
2018-05-23 10:08:56 +0000

    binding transformation frames en user control

--- trunk/Prog/CoFiAppV1/CoFiAppV1/Frames/FilmDesc.xaml.cs
+++ trunk/Prog/CoFiAppV1/CoFiAppV1/Frames/FilmDesc.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Metier;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,33 @@ namespace CoFiAppV1.Frames
     /// <summary>
     /// Logique d'interaction pour FilmDesc.xaml
     /// </summary>
-    public partial class FilmDesc : Page
+    public partial class FilmDesc : UserControl
     {
+        public Manager LeManager
+        {
+            get
+            {
+                return (Application.Current as App).LeManager;
+            }
+        }
         public FilmDesc()
         {
             InitializeComponent();
+            DataContext = LeManager;
         }
+
+
+
+        public string Titre
+        {
+            get { return (string)GetValue(TitreProperty); }
+            set { SetValue(TitreProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Titre.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitreProperty =
+            DependencyProperty.Register("Titre", typeof(string), typeof(FilmDesc), new PropertyMetadata("sans titre"));
+
+
     }
 }
