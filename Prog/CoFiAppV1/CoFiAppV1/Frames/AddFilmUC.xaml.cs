c4259ede168b2b0150150e546fa5225f5b4c13c4
clallavena <clallavena@1ddcc0a7-e0ef-4e35-b477-2a29d5999e86>
2018-06-05 22:00:38 +0000

    Début d'ajout de film, debug du lostfocus

--- Prog/CoFiAppV1/CoFiAppV1/Frames/AddFilmUC.xaml.cs
+++ Prog/CoFiAppV1/CoFiAppV1/Frames/AddFilmUC.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Metier;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,14 +24,69 @@ namespace CoFiAppV1.Frames
     {
         public NavigationManager NavManager => (Application.Current as App).NavManager;
 
+        public Manager LeManager
+        {
+            get
+            {
+                return (Application.Current as App).LeManager;
+            }
+        }
+
         public AddFilmUC()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
         private void Accueil_Click(object sender, RoutedEventArgs e)
         {
             NavManager.SelectedPart = NavManager.Parts["Accueil"]();
+        }
+
+        private void Ajouter_Click(object sender, RoutedEventArgs e)
+        {
+            string Titre;
+            int Sortie;
+            string Synopsis;
+            Personne buff;
+            List<Personne> li = new List<Personne>();
+            Dictionary<Job, List<Personne>> personnesAdd = new Dictionary<Job, List<Personne>>();
+
+            if (!string.IsNullOrEmpty(TitreBox.Text))
+            {
+                Titre = TitreBox.Text;
+
+                try
+                {
+                   Sortie = Int32.Parse(DateDeSortieBox.Text);
+                }
+                catch (FormatException ex)
+                {
+                    MessageBox.Show(ex.Message, "Erreur Format Date de Sortie", MessageBoxButton.OK);
+                    return;
+                }
+
+                Synopsis = SynopsisBox.Text;
+
+                foreach (Personne p in LeManager.ListReal)
+                {
+                    if (p.Nom.Contains(ComboReal.Text))
+                    {
+                        buff = new Personne(p);
+                        li.Add(buff);
+                        personnesAdd.Add(Job.Realisateur, li);
+                        return;
+                    }
+
+                }
+
+                Film f = new Film(Titre, Sortie, Synopsis, personnesAdd, Metier.Tag.Action);
+                LeManager.AjouterFilm(f);
+            }
+            else
+            {
+                MessageBox.Show("Le titre n'a pas été renseigné", "Erreur Ajout", MessageBoxButton.OK);
+            }
         }
     }
 }
