2ebd08cecfbd266cd9d8a22ca5050cec775c545e
esbarland <esbarland@1ddcc0a7-e0ef-4e35-b477-2a29d5999e86>
2018-06-13 14:30:56 +0000

    modif real fonctionnel mais à faire ne référence sur les réalisateurs et non par copie, modif image film marche mais ne s'update pas dans la listebox

--- trunk/Prog/CoFiAppV1/CoFiAppV1/Frames/ModifRealUC.xaml.cs
+++ trunk/Prog/CoFiAppV1/CoFiAppV1/Frames/ModifRealUC.xaml.cs
@@ -31,15 +31,69 @@ namespace CoFiAppV1.Frames
             }
         }
 
+        public List<int> Jours { get; set; }
+        public List<int> Mois { get; set; }
+
+        public int NaissanceJour { get; set; }
+        public int NaissanceMois { get; set; }
+        public int NaissanceAnnee { get; set; }
+        public int MortJour { get; set; }
+        public int MortMois { get; set; }
+        public int MortAnnee { get; set; }
+
         public ModifRealUC()
         {
             InitializeComponent();
+            initDate();
+            DataContext = this;
+        }
+
+        private void initDate()
+        {
+            Jours = new List<int>();
+            Mois = new List<int>();
+            for (int i = 1; i < 32; i++)
+            {
+                Jours.Add(i);
+            }
+            for (int i = 1; i < 13; i++)
+            {
+                Mois.Add(i);
+            }
+
+            NaissanceJour = LeManager.RealSelected.DateDeNaissance.Day;
+            NaissanceMois = LeManager.RealSelected.DateDeNaissance.Month;
+            NaissanceAnnee = LeManager.RealSelected.DateDeNaissance.Year;
+            if (LeManager.RealSelected.DateDeMort != null)
+            {
+                MortJour = LeManager.RealSelected.DateDeMort.Value.Day;
+                MortMois = LeManager.RealSelected.DateDeMort.Value.Month;
+                MortAnnee = LeManager.RealSelected.DateDeMort.Value.Year;
+            }
+
         }
 
         private void Accueil_Click(object sender, RoutedEventArgs e)
         {
             NavManager.SelectedPart = NavManager.Parts["Accueil"]();
             LeManager.FilmSelected = null;
+        }
+
+        private void ModifReal_Unloaded(object sender, RoutedEventArgs e)
+        {
+            DateTime nais = new DateTime(NaissanceAnnee, NaissanceMois, NaissanceJour);
+            DateTime? mort = new DateTime(MortAnnee, MortMois, MortJour);
+
+            if (LeManager.RealSelected.DateDeNaissance != nais)
+            {
+                LeManager.RealSelected.DateDeNaissance = nais;
+            }
+
+            if (LeManager.RealSelected.DateDeMort != mort)
+            {
+                LeManager.RealSelected.DateDeMort = mort;
+            }
+
         }
     }
 }
